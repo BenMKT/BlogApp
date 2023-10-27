767c101ac1fcd1e5215e4f4ff39af0781fff8661
Benson Kiai <benson.kiai86@gmail.com>
2023-10-27 21:54:30 +0300

    Add  comment model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  # Validations
+  validates :post_id, presence: true
+  validates :text, presence: true, length: { maximum: 1000 }
+
   # Callback
   after_save :updates_comments_counter
 
