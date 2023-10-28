fd5ea5530419a7676dc8190e2fae5a764f0c0835
Benson Kiai <benson.kiai86@gmail.com>
2023-10-28 08:27:19 +0300

    Add respective missing model methods tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe Like, type: :model do
     like = Like.new(post_id: post.id, user_id: user.id)
     expect(like).to be_valid
   end
+
+  it 'should update post likes counter to 2' do
+    user = User.create(name: 'Jonh Doe', posts_counter: 0)
+    post = Post.create(title: 'post1', author_id: user.id, comments_counter: 0, likes_counter: 0)
+    like = Like.create(user_id: user.id, post_id: post.id)
+    like2 = Like.create(user_id: user.id, post_id: post.id)
+    like.updates_post_likes_counter
+    expect(post.reload.likes_counter).to eq(2)
+  end
 end
