10adbd030acb961bcb5f109f99f255eee6294d74
Benson Kiai <benson.kiai86@gmail.com>
2023-10-28 08:27:40 +0300

    Add respective missing model methods tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -22,4 +22,27 @@ RSpec.describe Post, type: :model do
     post = Post.new(title: 'Test Post', comments_counter: 0, likes_counter: 1.5, author_id: user.id)
     expect(post).to_not be_valid
   end
+
+  it 'increments the author\'s posts_counter when a post is saved' do
+    user = User.create(name: 'John', posts_counter: 0)
+    post = Post.new(title: 'Test Post', comments_counter: 0, likes_counter: 0, author_id: user.id)
+    expect {
+      post.save
+      user.reload
+    }.to change(user, :posts_counter).by(1)
+  end
+
+  it 'returns the most recent comments limited by the specified limit' do
+    user = User.create(name: 'John', posts_counter: 0)
+    post = Post.new(title: 'Test Post', comments_counter: 0, likes_counter: 0, author_id: user.id)
+    post.save!
+    comments = []
+    (1..10).each do |i|
+      comment = Comment.create!(text: "Comment #{i}", user_id: user.id, post_id: post.id)
+      comments << comment
+    end
+
+    five_most_recent_comments = post.five_most_recent_comments
+    expect(five_most_recent_comments.to_a).to eq(comments.last(5).reverse)
+  end
 end
