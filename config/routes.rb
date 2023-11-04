ee3d6796ea186e4dfcbab289c5b4d55096cccdf8
Benson Kiai <benson.kiai86@gmail.com>
2023-11-04 03:29:37 +0300

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'users#index'
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 
-  resources :posts, only: [:show]
+  resources :posts, only: [:new, :create, :show] do
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create, :destroy]
+  end
 end
