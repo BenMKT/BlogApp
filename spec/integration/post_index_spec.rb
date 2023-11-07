aa31dca50468532b496bdfb291224a244a718224
Benson Kiai <benson.kiai86@gmail.com>
2023-11-07 22:15:41 +0300

    Create integration tests for the view files
    Co-Authored-By: Karim Barragan <karimdavid07@gmail.com>

---
+++ spec/integration/post_index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Post index page', type: :feature do
+  before do
+    @user = User.create(name: 'Ahmed', bio: 'Teacher', photo: 'https://thispeotexist.com/')
+    @post = @user.posts.create(title: 'Post title', text: 'Post body') # create a post
+  end
+
+  it "displays user and posts correctly" do
+    visit user_path(@user, @post)
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@post.title)
+    expect(page).to have_content(@post.text)
+    expect(page).to have_content(@post.comments_counter)
+    expect(page).to have_content(@post.likes_counter)
+    expect(page).to have_content(@user.posts_counter)
+    # Add more expectations for other elements
+  end
+
+  it "displays pagination when there are more posts than fit on one page" do
+  21.times { @user.posts.create(title: 'Post title', text: 'Post body') }
+  visit user_posts_path(@user)
+  expect(page).to have_selector('div.pagination')
+  end
+
+  it "redirects to post's show page when a post is clicked" do
+    visit user_posts_path(@user)
+    click_link @post.title
+    expect(page).to have_current_path(user_post_path(@user, @post))
+  end
+end
---
+++ spec/integration/user_show_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'User show page', type: :feature do
+    before do
+    @user = User.create(name: 'Benson', bio: 'Teacher', photo: 'https://thispeotexist.com/')
+    @post = @user.posts.create(title: 'Post title', text: 'Post body')
+
+    visit user_path(@user)
+    end
+
+    it 'displays the user profile picture' do
+        expect(page).to have_css(".profile-photo")
+    end
+
+    it 'displays the user username' do
+        expect(page).to have_content(@user.name)
+    end
+
+    it 'displays the number of posts the user has written' do
+        expect(page).to have_content(@user.posts_counter.to_s)
+    end
+
+    it 'displays the user bio' do
+        expect(page).to have_content(@user.bio)
+    end
+
+    it 'displays the user first 3 posts' do
+        5.times do |i|
+            @user.posts.create(title: "Post #{i}", text: "Post text #{i}")
+        end
+        visit user_path(@user)
+        @user.posts.order(created_at: :desc).first(3).each do |post|
+            expect(page).to have_content(post.title)
+            expect(page).to have_content(post.text)
+        end
+        expect(page).not_to have_content("Post 0")
+        expect(page).not_to have_content("Post 1")
+    end
+
+    it 'displays a button to view all of the user posts' do
+        expect(page).to have_link('See All Posts', href: user_posts_path(@user))
+    end
+
+    it "redirects to post's show page when a post is clicked" do
+        visit user_posts_path(@user)
+        click_link @post.title
+        expect(page).to have_current_path(user_post_path(@user, @post))
+    end
+
+    it 'redirects to the user posts index page when clicking on See all posts' do
+        click_link 'See All Posts'
+        expect(current_path).to eq(user_posts_path(@user))
+    end
+end
